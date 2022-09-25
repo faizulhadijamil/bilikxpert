f6a32c17ae1d90538d6474353b98fd39bc5cfc06
Faizul <faizulhadijamil@gmail.com>
2022-09-25 23:48:25 +0800

    updated gitignore and index

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
+'use strict';
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld3 = functions.https.onRequest((request, response) => {
+exports.helloWorldLocal = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+});
+
+exports.helloWorldLocal2 = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!2222", {structuredData: true});
+  response.send("Hello from Firebase!222");
 });
